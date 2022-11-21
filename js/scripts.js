e7b029728b16cdf9a8b5b0326965bc8495f68ae7
decentishDev <decentishdev@gmail.com>
2022-11-20 21:08:07 -0600

    creation

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("btn1").onclick = function(){
+    document.getElementById("outputText").innerHTML = document.getElementById("text1").value;
+    //document.getElementById("button").style.display = "none";
+}
