c8d7d4224609d55334e3994468c8f6de601362a9
Matthew Lundeen <97109488+decentishDev@users.noreply.github.com>
2022-11-21 12:27:53 -0600

    adding card values

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
+const sets = [["set11q1", "set11a1", "set11q2", "set11a2"], ["set12q1", "set12a1", "set12q2", "set12a2"]];
+let set = -1;
+let card = 0;
 document.getElementById("list11").onclick = function(){
     //document.getElementById("outputText").innerHTML = document.getElementById("text1").value;
     //document.getElementById("button").style.display = "none";
     //document.getElementById("list11").style = "background-color: rgb(70, 70, 70);";
-    startSet("list11");
+    startSet(11);
 }
 document.getElementById("list12").onclick = function(){
-    startSet("list12");
+    startSet(12);
 }
 document.getElementById("list13").onclick = function(){
-    startSet("list13");
+    startSet(13);
 }
 document.getElementById("list14").onclick = function(){
-    startSet("list14");
+    startSet(14);
 }
 document.getElementById("list15").onclick = function(){
-    startSet("list15");
+    startSet(15);
 }
 function startSet(listName){
     document.getElementById("list11").style = "background-color: rgb(39, 39, 39);";
@@ -22,5 +25,6 @@ function startSet(listName){
     document.getElementById("list13").style = "background-color: rgb(39, 39, 39);";
     document.getElementById("list14").style = "background-color: rgb(39, 39, 39);";
     document.getElementById("list15").style = "background-color: rgb(39, 39, 39);";
-    document.getElementById(listName).style = "background-color: rgb(70, 70, 70);";
+    document.getElementById("list" + listName.toString()).style = "background-color: rgb(70, 70, 70);";
+    document.getElementById("card").innerHTML = sets[(listName-10)[1]];
 }
