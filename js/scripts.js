222051653d3a308206124783ba4a42834c57e561
decentishDev <decentishdev@gmail.com>
2022-11-22 21:35:39 -0600

    card flipping

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,30 @@ document.getElementById("list14").onclick = function(){
 document.getElementById("list15").onclick = function(){
     startSet(15);
 }
+document.getElementById("know").onclick = function(){
+    if(card%2 == 0){
+        if(card == sets[set].length - 2){
+            document.getElementById("list1" + (set+1).toString()).style = "background-color: rgb(0, 255, 0);";
+        }else{
+            card+=2;
+        }
+    }else{
+        if(card == sets[set].length - 1){
+            document.getElementById("list11").style = "background-color: rgb(0, 255, 0);";
+        }else{
+            card++;
+        }
+    }
+    document.getElementById("card").innerHTML = sets[set][card];
+}
+document.getElementById("card").onclick = function(){
+    if(card % 2 == 0){
+        card++;
+    }else{
+        card--;
+    }
+    document.getElementById("card").innerHTML = sets[set][card];
+}
 function startSet(listName){
     document.getElementById("list11").style = "background-color: rgb(39, 39, 39);";
     document.getElementById("list12").style = "background-color: rgb(39, 39, 39);";
@@ -26,5 +50,11 @@ function startSet(listName){
     document.getElementById("list14").style = "background-color: rgb(39, 39, 39);";
     document.getElementById("list15").style = "background-color: rgb(39, 39, 39);";
     document.getElementById("list" + listName.toString()).style = "background-color: rgb(70, 70, 70);";
-    document.getElementById("card").value = sets[listName - 11][1];
+    document.getElementById("card").disabled = false;
+    document.getElementById("know").disabled = false;
+    document.getElementById("dont").disabled = false;
+    document.getElementById("card").style = "padding: 50px 10px; display: inline-block; margin: 0px 0px; font-size: 40px; color: rgb(255, 255, 255); box-sizing: content-box; width: 50%; height: 37%;";
+    document.getElementById("card").innerHTML = sets[listName - 11][0];
+    set = listName - 11;
+    card = 0;
 }
